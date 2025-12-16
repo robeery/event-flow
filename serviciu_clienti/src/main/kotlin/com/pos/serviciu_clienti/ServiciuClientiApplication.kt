c62617adb396eeb34eea2e879b57eb8e40ea8c48
Robert Constantin <robertconstantingrigoras@gmail.com>
2025-12-16 22:30:40 +0200

    lab03 - first steps into configuration for client service

---
+++ serviciu_clienti/src/main/kotlin/com/pos/serviciu_clienti/ServiciuClientiApplication.kt
@@ -0,0 +1,11 @@
+package com.pos.serviciu_clienti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiciuClientiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiciuClientiApplication>(*args)
+}
---
+++ serviciu_clienti/src/test/kotlin/com/pos/serviciu_clienti/ServiciuClientiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pos.serviciu_clienti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiciuClientiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
